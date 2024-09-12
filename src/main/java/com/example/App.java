d59381a0759996d60fd7397e998b953256c1b728
Michael <michael.fernandez@tecsup.edu.pe>
2024-09-11 22:54:04 -0500

    Añadir clase App

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -3,15 +3,9 @@ package com.example;
 /**
  * Hello world!
  */
-public final class App {
-    private App() {
-    }
-
-    /**
-     * Says hello to the world.
-     * @param args The arguments of the program.
-     */
+public class MFH {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hola, soy MFH.");
     }
 }
+
